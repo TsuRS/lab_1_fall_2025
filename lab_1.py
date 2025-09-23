2b082c485f76cf2388a590a793e450caf238ea70
Bahram Mohmand <bahram.mohmand123@gmail.com>
2025-09-23 15:19:16 -0700

    change kp and kd

--- lab_1.py
+++ lab_1.py
@@ -10,8 +10,8 @@ import signal
 JOINT_NAME = "leg_front_l_1"
 ####
 ####
-KP = 2.0  # YOUR KP VALUE
-KD = 0.3  # YOUR KD VALUE
+KP = 1.0  # YOUR KP VALUE
+KD = 0.5  # YOUR KD VALUE
 ####
 ####
 LOOP_RATE = 200  # Hz
